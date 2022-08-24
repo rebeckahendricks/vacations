291bc3b26136c2978b196de57b50ca14f022c0df
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-08-24 17:13:48 -0600

    Complete User Story 2

--- app/controllers/vacation_homes_controller.rb
+++ app/controllers/vacation_homes_controller.rb
@@ -2,4 +2,8 @@ class VacationHomesController < ApplicationController
   def index
     @vacation_homes = VacationHome.all
   end
+
+  def show
+    @vacation_home = VacationHome.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get '/', to: 'welcome#index'
 get '/vacation_homes', to: 'vacation_homes#index'
+get '/vacation_homes/:id', to: 'vacation_homes#show'
 end
