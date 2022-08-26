ae07513b8b7e1051a46fc18cee2c451749e90520
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-08-26 10:01:51 -0600

    Adds child index link

--- spec/features/bookings/index_spec.rb
+++ spec/features/bookings/index_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe 'Bookings index page' do
     @booking = @vacation_home.bookings.create!(checkin: "2022-01-21", checkout: "2022-01-24", guest_surname: "Hendricks", guest_firstname: "Rebecka", guests: 4, verified: true)
     @booking2 = @vacation_home.bookings.create!(checkin: "2022-01-25", checkout: "2022-01-29", guest_surname: "Whitehall", guest_firstname: "Corey", guests: 8, verified: true)
   end
+# As a visitor
+# When I visit any page on the site
+# Then I see a link at the top of the page that takes me to the Child Index
   describe 'as a visitor' do
     describe 'when I visit "/bookings"' do
       describe 'I see the all of the bookings in the system' do
@@ -25,6 +28,37 @@ RSpec.describe 'Bookings index page' do
           expect(page).to have_content(@booking2.guest_firstname)
           expect(page).to have_content(@booking2.guests)
           expect(page).to have_content(@booking2.verified)
+        end
+      end
+    end
+
+    describe 'when I visit any page on the site' do
+      describe 'I see a link at the top of the page that takes me to the bookings index' do
+        it 'has a link to bookings index on the top of each page' do
+          visit '/'
+          expect(page).to have_link("View all Bookings")
+          click_on "View all Bookings"
+          expect(current_path).to eq('/bookings')
+
+          visit '/vacation_homes'
+          expect(page).to have_link("View all Bookings")
+          click_on "View all Bookings"
+          expect(current_path).to eq('/bookings')
+
+          visit "/vacation_homes/#{@vacation_home.id}"
+          expect(page).to have_link("View all Bookings")
+          click_on "View all Bookings"
+          expect(current_path).to eq('/bookings')
+
+          visit "/bookings/#{@booking.id}"
+          expect(page).to have_link("View all Bookings")
+          click_on "View all Bookings"
+          expect(current_path).to eq('/bookings')
+
+          visit "/vacation_homes/#{@vacation_home.id}/bookings"
+          expect(page).to have_link("View all Bookings")
+          click_on "View all Bookings"
+          expect(current_path).to eq('/bookings')
         end
       end
     end
