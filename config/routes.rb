f2a0ab232ec617b53d2957bbdf58b9b24fe1e786
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-08-27 17:15:48 -0600

    Create booking update

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,14 @@ get '/vacation_homes/new', to: 'vacation_homes#new'
 post '/vacation_homes', to: 'vacation_homes#create'
 get '/vacation_homes/:id', to: 'vacation_homes#show'
 get '/vacation_homes/:id/edit', to: 'vacation_homes#edit'
-patch 'vacation_homes/:id', to: 'vacation_homes#update'
+patch '/vacation_homes/:id', to: 'vacation_homes#update'
 
 get '/bookings', to: 'bookings#index'
 get '/bookings/:id', to: 'bookings#show'
+get '/bookings/:id/edit', to: 'bookings#edit'
+patch '/bookings/:id', to: 'bookings#update'
 
 get '/vacation_homes/:id/bookings', to: 'vacation_homes_bookings#index'
-get 'vacation_homes/:id/bookings/new', to: 'vacation_homes_bookings#new'
-post 'vacation_homes/:id/bookings', to: 'vacation_homes_bookings#create'
+get '/vacation_homes/:id/bookings/new', to: 'vacation_homes_bookings#new'
+post '/vacation_homes/:id/bookings', to: 'vacation_homes_bookings#create'
 end
--- spec/features/vacation_homes/bookings/new_spec.rb
+++ spec/features/vacation_homes/bookings/new_spec.rb
@@ -1,14 +1,5 @@
 require 'rails_helper'
-# As a visitor
-# When I visit a Parent Children Index page
-# Then I see a link to add a new adoptable child for that parent "Create Child"
-# When I click the link
-# I am taken to '/parents/:parent_id/child_table_name/new' where I see a form to add a new adoptable child
-# When I fill in the form with the child's attributes:
-# And I click the button "Create Child"
-# Then a `POST` request is sent to '/parents/:parent_id/child_table_name',
-# a new child object/row is created for that parent,
-# and I am redirected to the Parent Childs Index page where I can see the new child listed
+
 RSpec.describe 'Parent Child Creation' do
   before :each do
     @vacation_home = VacationHome.create!(listing_name: "Gorgeous home", location: "San Jose, Costa Rica", rating:4.9, verified:true, guest_capacity:6)
@@ -46,7 +37,7 @@ RSpec.describe 'Parent Child Creation' do
         click_on 'Create Booking'
 
         expect(current_path).to eq("/vacation_homes/#{@vacation_home.id}/bookings")
-        
+
         expect(page).to have_content('Joe')
       end
     end
